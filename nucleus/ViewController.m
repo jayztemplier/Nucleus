c9ab412d90bcbee8dd4db904ab15ffac7b7f35b4
Jeremy Templier <templier.jeremy@gmail.com>
2015-07-15 11:27:30 -0700

    Initial Commit

---
+++ nucleus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  nucleus
+//
+//  Created by Jeremy Templier on 15/07/15.
+//  Copyright (c) 2015 Jeremy Templier. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
